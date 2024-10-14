c5beafa2dcdcb0f70d970e1f46f27ab16314360a
Frederic Linn <fredericlinn@gmail.com>
2024-10-14 15:27:37 +0200

    Add command line parsing and test cases
    The testcases still rely on hardcoded paths for now.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use clap::Parser;
 use eszip::EszipV2;
 use futures::io::BufReader;
 use futures::AsyncReadExt;
-use object::{Object, ObjectSection, ReadRef, Section};
+use object::{Object, ObjectSection};
 use serde::{Deserialize, Serialize};
+use std::env;
 use std::error::Error;
 use std::fs::{create_dir_all, read, File};
 use std::io::Write;
+use std::path::PathBuf;
 use std::path::{absolute, Path};
 use std::time::Instant;
 
@@ -42,7 +45,7 @@ pub struct VirtualSymlink {
 }
 
 #[derive(Debug)]
-struct Trailer {
+pub struct Trailer {
     eszip_pos: u64,
     metadata_pos: u64,
     npm_vfs_pos: u64,
@@ -82,13 +85,47 @@ impl Trailer {
     }
 }
 
+#[derive(Parser, Debug)]
+#[command(
+    name = "DenoDig",
+    version = "1.0",
+    author = "Frederic Linn",
+    about = "Parses --input and --output"
+)]
+struct Cli {
+    /// Input file path (required)
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Output file path (optional, defaults to the current executable's path)
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    // Get the output path: use the provided one, or fallback to the current working directory.
+    let mut output_path = args
+        .output
+        .unwrap_or_else(|| env::current_dir().expect("Failed to get the current executable path"));
+
+    output_path = output_path.join("excavated");
+
+    create_dir_all(&output_path)?;
+    process_binary_file(&args.input, &output_path).await?;
+
+    Ok(())
+}
+
+async fn process_binary_file(
+    input_path: &Path,
+    output_directory: &Path,
+) -> Result<(), Box<dyn Error>> {
     let timer = Instant::now();
 
-    // let binary_data = read("/Users/fli/Git/DenoDig/test/hello-v1.7.exe")?;
-    // let binary_data = read("/Users/fli/Git/DenoDig/test/telecraft_deno_1.44")?;
-    let binary_data = read("/Users/fli/Git/DenoDig/test/telecraft_deno_1.46")?;
+    let binary_data = read(input_path).expect("Failed to open file");
 
     if check_version(&binary_data, VERSION_UNO_OFFSET) {
         println!("Binary compiled with Deno >=1.6.0  <1.7.0");
@@ -97,11 +134,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         let bundle = &binary_data[bundle_pos as usize..&binary_data.len() - VERSION_UNO_OFFSET];
 
-        write_to_file(
-            Path::new("/Users/fli/Git/DenoDig/test/extracted/bundle.js"),
-            bundle,
-        )
-        .unwrap();
+        write_to_file(&output_directory.join("bundle.js"), bundle).unwrap();
     } else if check_version(&binary_data, VERSION_DOS_OFFSET) {
         println!("Binary compiled with Deno >=1.7.0  <1.33.3");
         let pointers: &[u8; 16] = &binary_data[(binary_data.len() - 16)..].try_into()?;
@@ -116,16 +149,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let bundle = &binary_data[bundle_pos as usize..metadata_pos as usize];
         let metadata = &binary_data[metadata_pos as usize..binary_data.len() - VERSION_DOS_OFFSET];
 
-        write_to_file(
-            Path::new("/Users/fli/Git/DenoDig/test/extracted/bundle.js"),
-            bundle,
-        )
-        .unwrap();
-        write_to_file(
-            Path::new("/Users/fli/Git/DenoDig/test/extracted/metadata.json"),
-            metadata,
-        )
-        .unwrap();
+        write_to_file(&output_directory.join("bundle.js"), bundle).unwrap();
+        write_to_file(&output_directory.join("metadata.json"), metadata).unwrap();
     } else if check_version(&binary_data, VERSION_TRES_OFFSET) {
         println!("Binary compiled with Deno >=1.33.3  <1.46");
         let trailer_data = &binary_data[binary_data.len() - TRAILER_SIZE..];
@@ -146,23 +171,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .await
             .unwrap();
 
-        let base_directory = Path::new("/Users/fli/Git/DenoDig/test/extracted");
+        write_to_file(&output_directory.join("metadata.json"), metadata.as_bytes()).unwrap();
 
-        write_to_file(
-            Path::new("/Users/fli/Git/DenoDig/test/extracted/metadata.json"),
-            metadata.as_bytes(),
-        )
-        .unwrap();
-
-        extract_modules(eszip, base_directory).await.unwrap();
-
-        // The offsets in the pre-1.46 versions are from the beginning of the file, which means
-        // have to pass a reference to the whole binary here.
-        extract_packages(&trailer, &binary_data)?;
+        extract_modules(eszip, output_directory).await.unwrap();
+        extract_packages(&trailer, &binary_data, output_directory)?;
     } else {
         println!("Binary compiled with Deno >= 1.46");
-        // Deno uses an object file section to store the application data since version 1.46.
-        // The presence of it determines our method of digging into the binary.
         let file = object::File::parse(&*binary_data)?;
         let section = file.section_by_name("d3n0l4nd").unwrap();
         println!("Found section '{}'", section.name()?);
@@ -170,18 +184,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let data = section.data()?;
         let trailer = Trailer::parse(&data[0..TRAILER_SIZE])?.unwrap();
 
-        // From now on we need to get rid of the leading trailer in order to calculate the correct offsets.
         let without_trailer = &data[TRAILER_SIZE..];
 
-        if cfg!(debug_assertions) {
-            println!("Deno trailer structure: {:?}", trailer);
-        }
-
         let bufreader = BufReader::new(without_trailer);
-
-        // "Once this function returns, the data section will not necessarially have been parsed yet.
-        // To parse the data section, poll/await the future returned in the second tuple slot."
-        // https://docs.rs/eszip/0.79.1/eszip/v2/struct.EszipV2.html#method.parse
         let (eszip, loader) = EszipV2::parse(bufreader).await?;
         let bufreader = loader.await?;
 
@@ -195,12 +200,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         println!("metadata: {}", metadata);
 
-        let base_directory = Path::new("/Users/fli/Git/DenoDig/test/extracted");
-
-        extract_modules(eszip, base_directory).await.unwrap();
-
-        // Trying extract the packages is safe, because the virtual file system is `null` in case no packages are used.
-        extract_packages(&trailer, &without_trailer)?;
+        write_to_file(&output_directory.join("metadata.json"), metadata.as_bytes()).unwrap();
+        extract_modules(eszip, output_directory).await.unwrap();
+        extract_packages(&trailer, &without_trailer, output_directory)?;
     }
 
     println!("🦖 digging took : {}s", timer.elapsed().as_secs_f64());
@@ -208,16 +210,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn extract_modules(eszip: EszipV2, base_directory: &Path) -> std::io::Result<()> {
+async fn extract_modules(eszip: EszipV2, output_directory: &Path) -> std::io::Result<()> {
     for specifier in eszip.specifiers().iter() {
         println!("Handling module '{}'", specifier);
 
         if let Some(module) = eszip.get_module(specifier) {
             let source = module.source().await.unwrap();
-            let file_path = base_directory.join(specifier);
+            let file_path = output_directory.join(specifier);
             let absolute_path = absolute(file_path)?;
 
-            if !absolute_path.starts_with(base_directory) {
+            if !absolute_path.starts_with(output_directory) {
                 panic!("Path traversal detected")
             }
 
@@ -233,7 +235,11 @@ async fn extract_modules(eszip: EszipV2, base_directory: &Path) -> std::io::Resu
     Ok(())
 }
 
-pub fn extract_packages(trailer: &Trailer, without_trailer: &[u8]) -> Result<(), Box<dyn Error>> {
+pub fn extract_packages(
+    trailer: &Trailer,
+    without_trailer: &[u8],
+    output_directory: &Path,
+) -> Result<(), Box<dyn Error>> {
     let vfs_data = &without_trailer[trailer.npm_vfs_pos as usize..trailer.npm_files_pos as usize];
 
     // TODO: Put this behind a verbose or debug flag.
@@ -247,7 +253,7 @@ pub fn extract_packages(trailer: &Trailer, without_trailer: &[u8]) -> Result<(),
     if let Some(virtual_directory) = vfs_root {
         let npm_files = &without_trailer[trailer.npm_files_pos as usize..];
 
-        traverse_directories(virtual_directory, npm_files, "")?;
+        traverse_directories(virtual_directory, npm_files, output_directory, "")?;
     };
 
     Ok(())
@@ -256,10 +262,9 @@ pub fn extract_packages(trailer: &Trailer, without_trailer: &[u8]) -> Result<(),
 pub fn traverse_directories(
     dir: VirtualDirectory,
     npm_files: &[u8],
+    output_directory: &Path,
     parent_path: &str,
 ) -> Result<(), Box<dyn Error>> {
-    let base_directory = Path::new("/Users/fli/Git/DenoDig/test/extracted");
-
     let current_path = if parent_path.is_empty() {
         dir.name.clone()
     } else {
@@ -273,11 +278,11 @@ pub fn traverse_directories(
 
                 let file_bytes = &npm_files[offset as usize..(offset + file.len) as usize];
 
-                let file_path = base_directory.join(&current_path).join(file.name);
+                let file_path = output_directory.join(&current_path).join(file.name);
 
                 let absolute_path = absolute(&file_path).unwrap();
 
-                if !absolute_path.starts_with(base_directory) {
+                if !absolute_path.starts_with(output_directory) {
                     panic!("Path traversal detected")
                 }
 
@@ -288,7 +293,7 @@ pub fn traverse_directories(
                 write_to_file(absolute_path, file_bytes)?;
             }
             VfsEntry::Dir(sub_dir) => {
-                traverse_directories(sub_dir, npm_files, &current_path)?;
+                traverse_directories(sub_dir, npm_files, output_directory, &current_path)?;
             }
             VfsEntry::Symlink(_symlink) => {
                 panic!("Can't handle symlinks yet")
@@ -312,4 +317,89 @@ fn check_version(binary_data: &[u8], offset: usize) -> bool {
 fn write_to_file<P: AsRef<Path>>(path: P, content: &[u8]) -> std::io::Result<()> {
     let mut file = File::create(path)?;
     file.write_all(content)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::exists;
+    use tempfile::tempdir;
+
+    #[tokio::test]
+    async fn test_version_uno() {
+        let temp_dir = tempdir().unwrap();
+        let temp_path = temp_dir.path();
+
+        process_binary_file(
+            Path::new("/Users/fli/Git/DenoDig/test/hello-v1.6.exe"),
+            temp_path,
+        )
+        .await
+        .unwrap();
+
+        // Version uno only produces a bundle.js file.
+        let bundle_path = temp_path.join("bundle.js");
+
+        assert!(exists(bundle_path).unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_version_dos() {
+        let temp_dir = tempdir().unwrap();
+        let temp_path = temp_dir.path();
+
+        process_binary_file(
+            Path::new("/Users/fli/Git/DenoDig/test/hello-v1.7.exe"),
+            temp_path,
+        )
+        .await
+        .unwrap();
+
+        // Version dos produces a bundle and a metadata file.
+        let bundle_path = temp_path.join("bundle.js");
+        let metadata_path = temp_path.join("metadata.json");
+
+        assert!(exists(bundle_path).unwrap());
+        assert!(exists(metadata_path).unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_version_tres() {
+        let temp_dir = tempdir().unwrap();
+        let temp_path = temp_dir.path();
+
+        process_binary_file(
+            Path::new("/Users/fli/Git/DenoDig/test/telecraft_deno_1.44"),
+            temp_path,
+        )
+        .await
+        .unwrap();
+
+        // Version tres produces a node_modules folder, a source directory and a metadata file.
+        let metadata_path = temp_path.join("metadata.json");
+        let modules_path = temp_path.join("node_modules");
+
+        assert!(exists(metadata_path).unwrap());
+        assert!(exists(modules_path).unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_version_quatro() {
+        let temp_dir = tempdir().unwrap();
+        let temp_path = temp_dir.path();
+
+        process_binary_file(
+            Path::new("/Users/fli/Git/DenoDig/test/telecraft_deno_1.46"),
+            temp_path,
+        )
+        .await
+        .unwrap();
+
+        // Version quatro produces a node_modules folder, a source directory and a metadata file.
+        let metadata_path = temp_path.join("metadata.json");
+        let modules_path = temp_path.join("node_modules");
+
+        assert!(exists(metadata_path).unwrap());
+        assert!(exists(modules_path).unwrap());
+    }
 }
